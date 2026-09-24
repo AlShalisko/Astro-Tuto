03af94a75f4d8b5430a5412afb34f7c30c9eafd1
AlShalisko <165971204+AlShalisko@users.noreply.github.com>
2026-09-24 13:28:38 -0600

    Stage 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-tuto-23300737.netlify.app/"
+  site: "https://astro-tuto-23300737.netlify.app/",
+  integrations: [preact()]
 });
